ddf3efbdfbe5626c0153dc597f5aa2ec422702f6
levonasatryan <levonasatryan@doublecoconut.com>
2019-03-25 13:52:33 +0400

    fix for shitty code

--- src/ui/GameScreen.ts
+++ src/ui/GameScreen.ts
@@ -4,70 +4,67 @@ import {Player} from './components/Player';
 
 
 export class GameScreen extends BaseScreen {
-    private background: Phaser.TileSprite;
-    private readonly cactusAssets: string[] = [
-        Images.ImagesBigCactus.getName(),
-        Images.ImagesCactus.getName(),
-        Images.ImagesCactus2.getName(),
-        Images.ImagesCactus3.getName(),
-        Images.ImagesCactusGroup.getName()
-    ];
-    private cactusArray: Phaser.Sprite[] = [];
-    private dino: Player;
-    private removed: Phaser.Sprite[] = [];
+	private background: Phaser.TileSprite;
+	private readonly cactusAssets: string[] = [
+		Images.ImagesBigCactus.getName(),
+		Images.ImagesCactus.getName(),
+		Images.ImagesCactus2.getName(),
+		Images.ImagesCactus3.getName(),
+		Images.ImagesCactusGroup.getName()
+	];
+	private cactusArray: Phaser.Sprite[] = [];
+	private dino: Player;
+	private removed: Phaser.Sprite[] = [];
 
-    constructor(game: Phaser.Game, parent: PIXI.DisplayObjectContainer) {
-        super(game, parent);
-        this.initialize();
-    }
+	constructor(game: Phaser.Game, parent: PIXI.DisplayObjectContainer) {
+		super(game, parent);
+		this.initialize();
+	}
 
-    private initialize(): void {
-        this.initBackground();
-        this.initCactuses();
-        this.initPlayer();
-    }
+	private initialize(): void {
+		this.initBackground();
+		this.initCactuses();
+		this.initPlayer();
+	}
 
-    private initBackground(): void {
-        this.background = this.game.add.tileSprite(0, 450, 800, 33, Images.ImagesGround.getName());
-        this.add(this.background);
-    }
+	private initBackground(): void {
+		this.background = this.game.add.tileSprite(0, 450, 800, 33, Images.ImagesGround.getName());
+		this.add(this.background);
+	}
 
-    private initCactuses(): void {
-        this.initCactus();
-        this.game.time.events.loop(Phaser.Timer.SECOND * 2, () => {
-            this.game.time.events.add(Phaser.Timer.SECOND * (Math.floor(Math.random())), this.initCactus, this);
-        }, this);
-    }
+	private initCactuses(): void {
+		this.initCactus();
+		this.game.time.events.loop(Phaser.Timer.SECOND * 2, () => {
+			this.game.time.events.add(Phaser.Timer.SECOND * (Math.floor(Math.random())), this.initCactus, this);
+		}, this);
+	}
 
-    private initCactus(): void {
-        const cactus = this.game.add.sprite(800, 450, this.cactusAssets[Math.floor(Math.random() * 5)], null, this);
-        cactus.y -= cactus.height;
-        this.cactusArray.push(cactus);
-    }
+	private initCactus(): void {
+		const cactus = this.game.add.sprite(800, 450, this.cactusAssets[Math.floor(Math.random() * 5)], null, this);
+		cactus.y -= cactus.height;
+		this.cactusArray.push(cactus);
+	}
 
-    private initPlayer(): void {
-        this.dino = new Player(this.game, 15, 358, Images.ImagesPlayerStand.getName(),  this);
-    }
+	private initPlayer(): void {
+		this.dino = new Player(this.game, 15, 358, Images.ImagesPlayerStand.getName(), this);
+	}
 
-    update() {
-        super.update();
-        this.background.tilePosition.x -= 5;
-        this.cactusArray.forEach((cactus) => {
-            cactus.x -= 5;
-        });
-            if (this.cactusArray[0].x + this.cactusArray[0].width >= this.dino.x) {
-                    this.removed = this.cactusArray.splice(0, 1);
-                    this.removed[0].x -= 5;
-
-            }
-            if (this.removed[0].x + this.removed[0].width <= 0) {
-                this.removed[0].destroy();
-            }
-        if (this.dino.x + this.dino.width >= this.cactusArray[0].x + this.cactusArray[0].width ||
-            this.dino.y + this.dino.height >= this.cactusArray[0].y + this.cactusArray[0].height) {
-            console.log('Game Over');
-        }
-    }
+	update() {
+		super.update();
+		this.background.tilePosition.x -= 5;
+		this.cactusArray.forEach((cactus) => cactus.x -= 5);
+		if (this.cactusArray[0].x + this.cactusArray[0].width <= this.dino.x) {
+			this.removed = this.cactusArray.splice(0, 1);
+			this.removed[0].x -= 5;
+		}
+		if (this.removed[0] && this.removed[0].x + this.removed[0].width <= 0) {
+			this.removed[0].destroy();
+		}
+		if (this.cactusArray[0] && (this.dino.x + this.dino.width >= this.cactusArray[0].x + this.cactusArray[0].width ||
+			this.dino.y + this.dino.height >= this.cactusArray[0].y + this.cactusArray[0].height)) {
+			console.log('Game Over');
+		}
+	}
 }
 
 
--- src/ui/components/Player.ts
+++ src/ui/components/Player.ts
@@ -1,46 +1,49 @@
 export class Player extends Phaser.Sprite {
-    private jumpButton: Phaser.Key;
-    private jump: boolean;
-    private fall: boolean;
-    private gravSpeed: number;
-    private jumpSpeed: number;
-    constructor(game: Phaser.Game, x: number, y: number, key: string, parent: PIXI.DisplayObjectContainer) {
-        super(game, x, y, key);
-        parent.addChild(this);
-        this.init();
-    }
-    private init(): void {
-        this.initDino();
-    }
-    private dinoJump(): void {
-        if (this.y >= 358) {
-            this.jump = true;
-            this.gravSpeed = 1;
-            this.jumpSpeed = 20;
-        }
-    }
+	private jumpButton: Phaser.Key;
+	private jump: boolean;
+	private fall: boolean;
+	private gravSpeed: number;
+	private jumpSpeed: number;
 
-    private initDino (): void {
-        this.inputEnabled = true;
-        this.jumpButton = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-        this.jumpButton.onDown.add(this.dinoJump, this);
-    }
+	constructor(game: Phaser.Game, x: number, y: number, key: string, parent: PIXI.DisplayObjectContainer) {
+		super(game, x, y, key);
+		parent.addChild(this);
+		this.init();
+	}
 
-    update() {
-        if (this.jump) {
-            this.y -= this.jumpSpeed;
-            this.jumpSpeed -= this.gravSpeed;
-        }
-            if (this.jumpSpeed <= 0) {
-            this.jump = false;
-            this.fall = true;
-        }
-        if (this.fall) {
-            this.jumpSpeed += this.gravSpeed;
-            this.y += this.jumpSpeed;
-        }
-        if (this.y >= 358) {
-            this.fall = false;
-        }
-        }
+	private init(): void {
+		this.initDino();
+	}
+
+	private dinoJump(): void {
+		if (this.y >= 358) {
+			this.jump = true;
+			this.gravSpeed = 1;
+			this.jumpSpeed = 20;
+		}
+	}
+
+	private initDino(): void {
+		this.inputEnabled = true;
+		this.jumpButton = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		this.jumpButton.onDown.add(this.dinoJump, this);
+	}
+
+	update() {
+		if (this.jump) {
+			this.y -= this.jumpSpeed;
+			this.jumpSpeed -= this.gravSpeed;
+		}
+		if (this.jumpSpeed <= 0) {
+			this.jump = false;
+			this.fall = true;
+		}
+		if (this.fall) {
+			this.jumpSpeed += this.gravSpeed;
+			this.y += this.jumpSpeed;
+		}
+		if (this.y >= 358) {
+			this.fall = false;
+		}
+	}
 }
